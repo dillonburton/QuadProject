efc3b2ab705cc1d4131be2a2334baf7dcd09c494
dillonburton <dilloncburton85@gmail.com>
2014-10-20 11:43:07 -0400

    Formatted stories

--- src/main.c
+++ src/main.c
@@ -6,7 +6,6 @@
 
 void start_input();
 double* start_calc(double, double, double);
-void start_tests();
 void tests_fixture_error_check();
 void test_error_check();
 
@@ -26,22 +25,10 @@ void test_error_check(){
     double* ans = start_calc(1,-3,-4);
     assert_double_equal(ans[0], -1, 0);
     assert_double_equal(ans[1], 4, 0);
+    assert_int_equal(1, 1);
 }
 
-int main(int argc, char **argv){
 
-	if(argv[1]){
-		start_tests();
-	}else{
-		start_input();
-	}
-
-    return 0;
-}
-
-void start_tests(){
-	run_tests(all_tests);
-}
 
 void start_input(){
     //get user input
@@ -66,4 +53,14 @@ double* start_calc(double a, double b, double c){
     return answers;
 }
 
+int main(int argc, char **argv){
+
+	if(argv[1]){
+		run_tests(all_tests);
+	}else{
+		start_input();
+	}
+
+    return 0;
+}
 
